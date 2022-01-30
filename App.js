384c3a7a1505a9edfe90b31e66926457b979904f
J-Zyzhu <1063821619@qq.com>
2022-01-30 02:54:21 -0500

    Add input and send button

--- App.js
+++ App.js
@@ -1,18 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 import Constants from "expo-constants";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-    </View>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      massage: " ",
+    };
+    
+    this.addItem = this.addItem.bind(this)
+  }
+
+  addItem () {
+    //firebase to send data
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style = {styles.msgBox}>
+          <TextInput
+            placeholder="Enter your massage"
+            onChangeText={(text) => this.setState({ message: text })}
+            style = {styles.txtInput}
+          />
+          <Button title="Send" onPress = {this.addItem}/>
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#eee',
+    backgroundColor: "#eee",
     marginTop: Constants.statusBarHeight,
+  },
+  msgBox: {
+    flexDirection: "row",
+    padding: 20,
+    backgroundColor: "#fff",
+  },
+  txtInput: {
+    flex: 1,
   },
 });
